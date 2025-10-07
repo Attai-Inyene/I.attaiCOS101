6b455464e26cac301342406349417373157c6df5
Attai-Inyene <inyene.attai@pau.edu.ng>
2025-10-07 18:33:54 +0100

    (My week 2 practice)

---
+++ week-1/practice_1.rs
@@ -0,0 +1,12 @@
+fn main() {
+	println!("welcome to CSC 101!");
+
+    println!("The course learnin outcome is as follows");
+
+	println!("1.Distinguish between compuer and computer programming,/n
+	          2.Develop some techniques i computer science,/n
+	          3.Understand the different areas of study in computer sciece,/n
+	          4.Conversant with applications of computeer science./n
+	          5.Navigate the career prospects in compuer science./n
+	          6.Conversant with computer programming concepts.");
+}
