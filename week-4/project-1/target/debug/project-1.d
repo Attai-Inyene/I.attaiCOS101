99214a2fe3a70ab1b4fe311822d665c76e19b7c6
Attai-Inyene <inyene.attai@pau.edu.ng>
2025-11-10 19:40:57 +0100

    (my week-7 practice)

--- week-4/project-1/target/debug/project-1.d
+++ week-4/project-1/target/debug/project-1.d
@@ -1,1 +1,1 @@
-C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\ca1\project-1\target\debug\project-1.exe: C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\ca1\project-1\src\main.rs
+C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\project-1\target\debug\project-1.exe: C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\project-1\src\main.rs
--- week-4/project-2/target/debug/project-2.d
+++ week-4/project-2/target/debug/project-2.d
@@ -1,1 +1,1 @@
-C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\ca1\project-2\target\debug\project-2.exe: C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\ca1\project-2\src\main.rs
+C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\project-2\target\debug\project-2.exe: C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-4\project-2\src\main.rs
