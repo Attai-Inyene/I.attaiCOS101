926cb53fe8277d14ab3f4390c04ce2626a8bf38f
Attai-Inyene <inyene.attai@pau.edu.ng>
2025-11-23 23:20:43 +0100

    (my week 8)

---
+++ week-8/project_2/target/debug/deps/project.d
@@ -0,0 +1,5 @@
+C:\Users\MY LAPTOP\Documents\I.attaiCOS101\week-8\project_2\target\debug\deps\project.d: src\main.rs
+
+C:\Users\MY LAPTOP\Documents\I.attaiCOS101\week-8\project_2\target\debug\deps\project.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/project_2/target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-8\project_2\target\debug\project.exe: C:\Users\MY\ LAPTOP\Documents\I.attaiCOS101\week-8\project_2\src\main.rs
